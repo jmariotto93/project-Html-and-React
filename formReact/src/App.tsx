bcc3b1f8e352bb9767cc03521f7579d8d831b02f
José Mariotto <josemarioto@gmail.com>
2023-01-04 23:42:56 -0300

    Formulário em react com estados inicio do tailwind

--- formReact/src/App.tsx
+++ formReact/src/App.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { Login } from "./app/pages/Login";
 
 function App() {
+  
   return (
     <div>
-      <Login text="Hello World" />
+      <Login />
     </div>
   );
 }
--- formReact/src/app/pages/Login/index.tsx
+++ formReact/src/app/pages/Login/index.tsx
@@ -1,13 +1,56 @@
-import './styles.css'
+import "./styles.css";
+import logoImg from "../../../assets/logo.jpg";
+import { useState } from "react";
 
-interface LoginProps {
-  text: string;
-}
+function Login() {
+  const [email, setEmail] = useState("");
+  // armazena o valor dos inputs
+  const [password, setPassword] = useState("");
 
-function Login({ text }:LoginProps) {
+  function hendleLogin(event: React.FormEvent<HTMLButtonElement>) {
+    event.preventDefault();
+    if (email == "jose@teste.com" && password == "1234") {
+      alert("email: " + email + " " + "password: " + password);
+    } else {
+      alert("Nome de usuário ou senha inválidos!");
+    }
+  }
+
   return (
-    <div className='container'>
-      <h3>{text}</h3>
+    <div className="container">
+      <main className="main">
+        <form className="form">
+          <h1>Welcome</h1>
+          <img className="logo" src={logoImg} alt="Logotipo" />
+
+          <section className="sectionInputs">
+            <input
+              onChange={(event) => setEmail(event.target.value)}
+              type="text"
+              id="email"
+              className="input"
+              required
+              placeholder="Email"
+            />
+            <input
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              id="password"
+              className="input"
+              required
+              placeholder="Password"
+            />
+
+            <button
+              className="bg-gradient-to-r from-sky-500 to-indigo-500 rounded-3xl text-white uppercase p-2"
+              type="submit"
+              onClick={hendleLogin}
+            >
+              Logar
+            </button>
+          </section>
+        </form>
+      </main>
     </div>
   );
 }
